580a2b1dabece029feab09224ac42ed3c633e5b7
yeison34k <yaristizabal34k@gmail.com>
2020-09-30 20:04:07 -0500

    Remove the now ignored directory node_modules

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
 const app = express();
 
-app.listen(3000, (err) => {
-    if(err) throw new Error();
-    console.log("") 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', client => {
+    client.on('disconnect', () => { 
+        console.log("cliente desconectado")
+    });
+
+    client.on('menasaje', () => {
+        console.log("Mensajessss")
+    }); 
+});
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath))
+
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error();
+    console.log("Hola mundo")
 });
