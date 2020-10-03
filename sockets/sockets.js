79219147ebaec8424e870841314a8ae267a87e83
yeison34k <yaristizabal34k@gmail.com>
2020-10-03 18:40:05 -0500

    add band list

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,11 +1,35 @@
-const { io } = require('../index')
+'use strict'
+
+const { io } = require('../index');
+
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+
+const bands = new Bands();
+
+bands.addBand(new Band("Asking Alexandria"))
+bands.addBand(new Band("No Doubt"))
+bands.addBand(new Band("Avenged Sevenfold"))
+bands.addBand(new Band("Grateful Dead"))
+bands.addBand(new Band("Kings of Leon"))
+bands.addBand(new Band("Alice in Chains"))
+bands.addBand(new Band("The Script"))
+
 
 io.on('connection', client => {
+    
     client.on('disconnect', () => { 
         console.log("cliente desconectado")
     });
 
     client.on('mensaje', (payload) => {
         io.emit("mensaje", {admin: "Nuevo MSM"})
-    }); 
+    });
+    
+    client.on('emitir-mensaje', (payload) => {
+        client.broadcast.emit("nuevo-mensaje", payload)
+    });
+
+    client.emit("active-bands", bands.getBands());
 });
